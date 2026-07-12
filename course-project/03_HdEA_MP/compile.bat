0ad13336b6de2143503a9236d17aac95e99ad28d
ricky-theseus <ricky_theseus@cug.edu.cn>
2026-07-12 19:23:49 +0800

    清理项目：删除旧代码/脚本/日志/build产物，保留 dEA+HdEA 两个实现和实验框架

--- course-project/03_HdEA_MP/compile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >nul 2>&1
-set "INC=C:\Program Files (x86)\Microsoft SDKs\MPI\Include\"
-set "LIBP=C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x64\"
-cl /EHsc /I"%INC%" "%~dp0TSP_HdEA_MP.cpp" /link /LIBPATH:"%LIBP%" msmpi.lib /OUT:"%~dp0TSP_HdEA_MP.exe"
-if %ERRORLEVEL% EQU 0 (echo BUILD SUCCESS) else (echo BUILD FAILED)
